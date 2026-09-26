2c34f81df8f3449b1ff67e4dd25e5bbdb5c27576
Yumis <52785831+Yumis84@users.noreply.github.com>
2026-09-26 08:00:08 +0200

    acceptance: pin OAuth client id

--- oauth/acceptance/config.js
+++ oauth/acceptance/config.js
@@ -2,5 +2,6 @@
 window.HUB_ACCEPTANCE_CONFIG={
  supabaseUrl:"https://trrhyahuzqbozxanaczw.supabase.co",
  supabasePublishableKey:"sb_publishable_gJ2s1m3TcfLlVymFoOHfkQ_iMxO7rB5",
+ oauthClientId:"81184c98-ffd9-4b53-b38c-776ca02fbc39",
  redirectUri:"https://yumis84.github.io/hub-web/oauth/acceptance/callback/"
 };
